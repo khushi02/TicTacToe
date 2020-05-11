606b086e845244aca8fdd49588c8f19603b23898
khushi02 <k9wadhwa@gmail.com>
2020-05-10 23:27:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ game = [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]]
 
-def display():
+def game_board(game_map, player=0, row=0, col=0, display=False):
+    if not display:
+        game_map[row][col] = player
+    
     print("   0  1  2")
-    for count, row in enumerate(game):
-        print(count, row)
+    for count, row in enumerate(game_map):
+        print(count, row)  
         
+    return game_map
+        
+game_board(game, 2, 1, 1)
